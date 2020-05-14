a0ed07dae7c0087530727bb925c2eb43d713ed5e
uditsharma16 <uditsharma16@yahoo.com>
2020-05-14 21:08:00 +1200

    Create Lab2.sql
    Lab 2 conditional searching

---
+++ Lab2.sql
@@ -0,0 +1,24 @@
+select * from employees e ; 
+----1
+select e.first_name||' '||e.last_name as "Full Name",j.job_title from employees e,jobs j 
+where e.job_id=j.job_id and e.salary>10000;
+----2
+select e.first_name||' '||e.last_name as "Full Name",j.job_title,e.salary from employees e,jobs j 
+where e.job_id=j.job_id and (e.salary not between 5000 and 12000) ;
+----3
+select e.employee_id,e.first_name||' '||e.last_name as "Full Name",j.job_title,e.hire_date as " Start Date"
+from employees e,jobs j
+where e.job_id=j.job_id 
+and lower(e.first_name||' '||e.last_name) in ('alyssa hutton','david austin');
+----4
+select l.street_address,l.postal_code,l.city from locations l where l.country_id in ('JP','IT');
+----5
+select e.first_name||' '||e.last_name as "Full Name",'Phone: '||e.phone_number||' Email: '||e.email as "Contant Details"
+from employees e
+where TO_DATE(e.hire_date, 'dd-MON-yy') between TO_DATE('01-JAN-96', 'dd-MON-yy') and TO_DATE('31-JAN-96', 'dd-MON-yy')
+order by e.last_name ;
+----7
+select e.first_name||' '||e.last_name as "Full Name",'Phone: '||e.phone_number||' Email: '||e.email as "Contant Details",e.hire_date
+from employees e
+order by hire_date desc;
+
